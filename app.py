93de3fa2a5831e3eabb1aaf2d0bbc7b8fea01a17
ujjwaltyagi2000 <ujjwaltyagi28@gmail.com>
2025-04-16 22:51:03 +0530

    deployment test 3

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-# import matplotlib.pyplot as plt
-# import seaborn as sns
+import matplotlib.pyplot as plt
+import seaborn as sns
 import plotly.express as px
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
